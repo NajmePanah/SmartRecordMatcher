dd63dc5526c1c310a0fb2cd84c955a6a6118c6a9
NajmePanah <n.panah1392@gmail.com>
2025-11-23 22:26:17 +0330

    initial design of MainForm

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            progressBar1 = new ProgressBar();
+            dataGridView1 = new DataGridView();
+            button4 = new Button();
+            button5 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(58, 43);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(300, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(58, 107);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(300, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(463, 43);
+            button1.Name = "button1";
+            button1.Size = new Size(212, 29);
+            button1.TabIndex = 2;
+            button1.Text = "انتخاب فایل معاملات";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(463, 107);
+            button2.Name = "button2";
+            button2.Size = new Size(212, 29);
+            button2.TabIndex = 3;
+            button2.Text = "انتخاب فایل کد اقتصادی";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(528, 199);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 4;
+            button3.Text = "پردازش";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(557, 256);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 5;
+            label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(58, 309);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(671, 29);
+            progressBar1.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(58, 380);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(671, 309);
+            dataGridView1.TabIndex = 7;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(432, 720);
+            button4.Name = "button4";
+            button4.Size = new Size(243, 29);
+            button4.TabIndex = 8;
+            button4.Text = "تأیید این تطبیق درست بود";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(135, 720);
+            button5.Name = "button5";
+            button5.Size = new Size(243, 29);
+            button5.TabIndex = 9;
+            button5.Text = "این تطبیق غلط بود";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 779);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(dataGridView1);
+            Controls.Add(progressBar1);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "MainForm";
+            Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private ProgressBar progressBar1;
+        private DataGridView dataGridView1;
+        private Button button4;
+        private Button button5;
     }
 }
--- obj/Debug/net8.0-windows/SmartRecordMatcher.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SmartRecordMatcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartRecordMatcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04cf27892813c717a373ce8f9cbfe4726efbecb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartRecordMatcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartRecordMatcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
